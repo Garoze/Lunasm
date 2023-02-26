3f9f69dd3d1ac095b54bc2478f0ada77d6b3fa80
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-02-26 10:53:10 -0300

    feat: add lexer file with constructor

---
+++ src/Lexer.cpp
@@ -0,0 +1,11 @@
+#include "Lexer.hpp"
+
+namespace Lunasm {
+
+Lexer::Lexer(const std::string& source_code)
+    : m_line(1)
+    , m_index(0)
+    , m_source_code(source_code)
+{}
+
+}  // namespace Lunasm
