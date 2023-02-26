4c726e8d83927513448df7e6965fbb76ca204569
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-02-26 10:49:56 -0300

    feat: add lexer header file
    only with the constructor for now

---
+++ include/Lexer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace Lunasm {
+
+class Lexer
+{
+public:
+		Lexer(const std::string&);
+private:
+    std::size_t m_line;
+    std::size_t m_index;
+    std::string m_source_code;
+};
+
+}  // namespace Lunasm
