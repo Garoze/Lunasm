8b59e7bb8cbd80c53f3d91cd56e7c6497cab944b
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-02-26 13:00:18 -0300

    feat: add print function implementation

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,19 +1,19 @@
-#include "Token.hpp"
+#include "fmt/core.h"
 
-#include "Mnemonics.hpp"
+#include "Token.hpp"
 
 namespace Lunasm {
 
 Token::Token(Kind kind, std::size_t line, std::size_t offset, std::string_view text)
-    : m_kind(kind)
+    : m_kind(Kind(kind))
     , m_line(line)
     , m_offset(offset)
     , m_text(text)
 {}
 
-std::string Token::as_string() const
+void Token::print() const
 {
-    return MNEMONICS.at(m_kind);
+    fmt::print("Token( T: {:8} Kind: {:18} Line: {:2}, Offset: {:2d} )\n", m_text, m_kind.as_string(), m_line, m_offset);
 }
 
 }  // namespace Lunasm
