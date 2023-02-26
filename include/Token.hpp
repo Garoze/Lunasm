0ab29aa5acfab8d35966565be57f6cf3937f95f6
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-02-26 11:12:16 -0300

    feat: add token header file

---
+++ include/Token.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+namespace Lunasm {
+
+enum class Kind {
+    NOP,
+};
+
+class Token
+{
+public:
+    Token(Kind, std::size_t, std::size_t, std::string_view);
+
+    std::string as_string() const;
+
+private:
+    Kind m_kind;
+    std::size_t m_line;
+    std::size_t m_offset;
+    std::string_view m_text;
+};
+
+}  // namespace Lunasm
