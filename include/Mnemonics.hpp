46e00dbfd7647130ef0f6ff03239bfd2a9f454db
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-03-01 21:53:30 -0300

    feat: add some more mnemonics
    added LoadImmediate, LoadAddress and EOF

--- include/Mnemonics.hpp
+++ include/Mnemonics.hpp
@@ -8,6 +8,13 @@ namespace Lunasm {
 
 const std::unordered_map<L16TokenKind, std::string> MNEMONICS = {
     {L16TokenKind::NOP, "NOP"},
+
+    {L16TokenKind::LoadImmediate, "LoadImmediate"},
+    {L16TokenKind::LoadAddress, "LoadAddress"},
+
+    {L16TokenKind::Register, "Register"},
+
+    {L16TokenKind::END, "EOF"},
 };
 
 }  // namespace Lunasm
