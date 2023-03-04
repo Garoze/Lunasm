15e6c32d3a90329129af669a12521f9210d58429
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-03-04 08:36:22 -0300

    feat: testing increment and decrement instructions

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    Lunasm::Lexer l("__start:ldi r1, $05\n__loop:\ncmi r1, $00\njnq [loop]");
+    Lunasm::Lexer l("__start:\nldi r1, $05\n__loop:\ndec r1\ncmi r1, $00\njnq [loop]");
 
     auto tokens = l.Tokenizer();
 
