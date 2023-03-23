eb5026ad59827cf19c505577684b37a4a55957b5
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-03-23 14:27:46 -0300

    feat: add argument parse using cxxopts
    TODO: finish all the options and implement the functions for each one

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,35 @@
-#include "fmt/core.h"
+#include <fmt/core.h>
+#include <cxxopts.hpp>
 
 #include "Lexer/Lexer.hpp"
 
-int main()
+const std::string VERSION = "Lunasm version 0.0.1";
+
+int main(int argc, char* argv[])
 {
-    Lunasm::Lexer l("start:\nlda r1, $05\nloop:\nsub r1, $01\ncmp r1, $00\njne [loop]\nhlt");
+    cxxopts::Options options("Lunasm", "\nAssembly language for the Luna16 fantasy console.\n");
 
-    auto tokens = l.Tokenizer();
+    // clang-format off
+    options.add_options()
+        ("f,file", "File name", cxxopts::value<std::string>())
+        ("R,repl", "Enable the REPL mode", cxxopts::value<bool>()->default_value("false"))
+        ("v,version", "Show the program version")
+        ("h,help", "Show usage menu");
+    // clang-format on
 
-    for (const auto& t : tokens)
-        t.print();
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+        fmt::print("{}", options.help());
+        std::exit(0);
+    }
+
+    if (result.count("version"))
+    {
+        fmt::print("Version: {}", VERSION);
+        std::exit(0);
+    }
 
     return EXIT_SUCCESS;
 }
