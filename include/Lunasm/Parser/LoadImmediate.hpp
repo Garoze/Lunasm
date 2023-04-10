01955cfb15db4b3c93f6ed2535e6281a0aadc338
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-04-10 07:50:39 -0300

    feat: add Register and Immediate to LoadImmediate class
    TODO: make it work

--- include/Lunasm/Parser/LoadImmediate.hpp
+++ include/Lunasm/Parser/LoadImmediate.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
+#include <memory>
+
 #include "Node.hpp"
+#include "Register.hpp"
+#include "Immediate.hpp"
 
 namespace Lunasm {
+
 class LoadImmediate : public Node
 {
 public:
-    LoadImmediate(std::size_t, std::uint8_t);
+    LoadImmediate(std::size_t, std::uint8_t, Register, Immediate);
 
     virtual void print() override;
     virtual std::uint8_t eval() override;
@@ -15,6 +20,8 @@ public:
 private:
     std::size_t m_size;
     std::uint8_t m_output;
+    Register m_register;
+    Immediate m_immediate;
 };
 
 }  // namespace Lunasm
--- src/Parser/LoadImmediate.cpp
+++ src/Parser/LoadImmediate.cpp
@@ -2,9 +2,11 @@
 
 namespace Lunasm {
 
-LoadImmediate::LoadImmediate(std::size_t size, std::uint8_t output)
+LoadImmediate::LoadImmediate(std::size_t size, std::uint8_t output, Register r, Immediate i)
     : m_size(size)
     , m_output(output)
+    , m_register(r)
+    , m_immediate(i)
 {}
 
 void LoadImmediate::print() {}
@@ -14,7 +16,8 @@ std::uint8_t LoadImmediate::eval()
     return m_output;
 }
 
-std::size_t LoadImmediate::size() const {
+std::size_t LoadImmediate::size() const
+{
     return m_size;
 }
 
