388c50bc6d52c6ec42ec46b0729fb3e1b80991a3
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-07-13 19:37:51 -0300

    feat: add the print and eval functions to the label class

--- include/Lunasm/Parser/Label.hpp
+++ include/Lunasm/Parser/Label.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <string_view>
+#include <vector>
 
 #include "Parser/Opcodes.hpp"
 
@@ -13,8 +14,11 @@ public:
     Label(std::string_view label);
     Label(std::string_view label, std::uint16_t address);
 
+    void print() const;
     void set_address(std::uint16_t address);
     void set_label(std::string_view label);
+
+    void eval(std::vector<std::uint8_t>& output) const;
 
     std::uint16_t address() const;
     std::string_view label() const;
--- src/Parser/Label.cpp
+++ src/Parser/Label.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+
+#include "fmt/core.h"
+
 #include "Parser/Label.hpp"
 
 namespace Lunasm {
@@ -11,6 +15,11 @@ Label::Label(std::string_view label, std::uint16_t address)
     : m_label(label)
     , m_address(address)
 {}
+
+void Label::print() const
+{
+    fmt::print("({} {})\n", m_label, m_address);
+}
 
 void Label::set_address(std::uint16_t address)
 {
@@ -30,6 +39,15 @@ std::uint16_t Label::address() const
 std::string_view Label::label() const
 {
     return m_label;
+}
+
+void Label::eval(std::vector<std::uint8_t>& output) const
+{
+    std::uint8_t LSB = (m_address & 0x00FF);
+    std::uint8_t MSB = (m_address & 0xFF00) >> 8;
+
+    output.push_back(LSB);
+    output.push_back(MSB);
 }
 
 } // namespace Lunasm
