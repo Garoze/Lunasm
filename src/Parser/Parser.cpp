f9f03862c55eae0042759d9b26d7b1b68930f587
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-07-13 17:53:26 -0300

    feat: add the div operation to the parser

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -459,9 +459,45 @@ void Parser::mul_instruction()
 void Parser::div_instruction()
 {
     expect(TokenKind::Div);
-    parse_register();
-    expect(TokenKind::Comma);
-    parse_modes();
+
+    switch (look_ahead()->kind())
+    {
+        case TokenKind::Register: {
+            std::uint8_t dst = parse_register();
+            expect(TokenKind::Comma);
+
+            switch (look_ahead()->kind())
+            {
+                case TokenKind::Immediate: {
+                    std::uint16_t src = parse_immediate();
+
+                    m_instructions.push_back(Instruction(Opcode::DivImmediate, 4, dst, src));
+                }
+                break;
+
+                case TokenKind::Register: {
+                    auto src = parse_register();
+
+                    m_instructions.push_back(Instruction(Opcode::DivRegister, 3, dst, src));
+                }
+                break;
+
+                case TokenKind::OpenBracket: {
+                    auto src = parse_address();
+
+                    m_instructions.push_back(Instruction(Opcode::DivAddress, 4, dst, src));
+                }
+                break;
+
+                default:
+                    break;
+            }
+        }
+        break;
+
+        default:
+            break;
+    }
 }
 
 void Parser::mod_instruction()
