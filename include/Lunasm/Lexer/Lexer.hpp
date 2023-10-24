03077743b6280bb510ba2088979771abd8f6e8b8
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-10-24 13:31:33 -0300

    refactore: add some nodiscard to lexer functions

--- include/Lunasm/Lexer/Lexer.hpp
+++ include/Lunasm/Lexer/Lexer.hpp
@@ -23,24 +23,25 @@ public:
 private:
     void step();
     void handle_space();
-    bool is_empty() const;
-    char current_char() const;
+
     std::optional<char> peek(std::size_t) const;
     std::string sanitize_input(std::string);
 
+    [[nodiscard]] bool is_empty() const;
+    [[nodiscard]] char current_char() const;
+
 private:
     void lex_comments();
-
-    Token lex_directives();
-    Token lex_separators();
-    Token lex_operators();
-    Token lex_registers();
-    Token lex_instructions();
-
     std::uint16_t get_base();
-    Token lex_immediates();
 
-    Token next_token();
+    [[nodiscard]] Token lex_directives();
+    [[nodiscard]] Token lex_separators();
+    [[nodiscard]] Token lex_operators();
+    [[nodiscard]] Token lex_registers();
+    [[nodiscard]] Token lex_instructions();
+    [[nodiscard]] Token lex_immediates();
+
+    [[nodiscard]] Token next_token();
 
 private:
     std::size_t m_line;
