c6711bc7158d15fb67f0a0df60973be7ea376faa
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-10-25 23:43:58 -0300

    feat: add Operand class and implementation
    this class is used as a nice abstraction for the std::variant on
    instructions

---
+++ include/Lunasm/Parser/Operand.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <string_view>
+#include <variant>
+
+namespace Parser {
+
+class Operand
+{
+public:
+    using value_t =
+        std::variant<std::monostate, std::uint16_t, std::string_view>;
+
+    Operand(value_t);
+
+    [[nodiscard]] value_t raw() const;
+    [[nodiscard]] std::string as_string() const;
+
+public:
+    constexpr explicit operator bool() noexcept
+    {
+        return !m_value.valueless_by_exception();
+    }
+
+private:
+    value_t m_value;
+};
+
+} // namespace Parser
---
+++ src/Parser/Operand.cpp
@@ -0,0 +1,37 @@
+#include "Parser/Operand.hpp"
+#include <string>
+
+namespace Parser {
+
+Operand::Operand(value_t value)
+    : m_value(value)
+{}
+
+Operand::value_t Operand::raw() const
+{
+    return m_value;
+}
+
+std::string Operand::as_string() const
+{
+    auto visitor = [&](auto& arg) -> std::string {
+        using T = std::decay_t<decltype(arg)>;
+
+        if constexpr (std::is_same_v<T, std::monostate>)
+        {
+            return "";
+        }
+        else if constexpr (std::is_same_v<T, std::uint16_t>)
+        {
+            return std::to_string(arg);
+        }
+        else if constexpr (std::is_same_v<T, std::string_view>)
+        {
+            return std::string{ arg };
+        }
+    };
+
+    return std::visit(visitor, m_value);
+}
+
+} // namespace Parser
