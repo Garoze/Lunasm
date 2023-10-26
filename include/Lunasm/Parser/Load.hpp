d6d29a79fe5e7cc7aff5ad72d26ddf0165ae02ef
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-10-26 00:59:18 -0300

    feat: add load instruction derive classes

---
+++ include/Lunasm/Parser/Load.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Parser/Instruction.hpp"
+#include "Parser/Operand.hpp"
+
+namespace Parser {
+
+class LoadImmediate : public Instruction
+{
+public:
+    LoadImmediate(Operand::value_t, Operand::value_t);
+
+    [[nodiscard]] kind_t raw() const;
+    [[nodiscard]] virtual std::string as_string() const override;
+
+private:
+    Operand m_dst;
+    Operand m_src;
+};
+
+class LoadRegister : public Instruction
+{
+public:
+    LoadRegister(Operand::value_t, Operand::value_t);
+
+    [[nodiscard]] kind_t raw() const;
+    [[nodiscard]] virtual std::string as_string() const override;
+
+private:
+    Operand m_dst;
+    Operand m_src;
+};
+
+class LoadAddress : public Instruction
+{
+public:
+    LoadAddress(Operand::value_t, Operand::value_t);
+
+    [[nodiscard]] kind_t raw() const;
+    [[nodiscard]] virtual std::string as_string() const override;
+
+private:
+    Operand m_dst;
+    Operand m_src;
+};
+
+} // namespace Parser
