1033c885b75ca87c1644ae0788c3f8383607e1f4
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-11-02 22:01:11 -0300

    refactore: removed all unused overloads

--- include/Lunasm/Assembler/Assembler.hpp
+++ include/Lunasm/Assembler/Assembler.hpp
@@ -10,51 +10,57 @@
 
 #include "Arch.hpp"
 
+// #define LIST_OF_INSTRUCTIONS                                                   \
+//     INST(Nop)                                                                  \
+//     INST(LoadImmediate)                                                        \
+//     INST(LoadAddress)                                                          \
+//     INST(LoadRegister)                                                         \
+//     INST(StoreImmediate)                                                       \
+//     INST(StoreAddress)                                                         \
+//     INST(StoreRegister)                                                        \
+//     INST(ShiftLeft)                                                            \
+//     INST(ShiftRight)                                                           \
+//     INST(And)                                                                  \
+//     INST(Or)                                                                   \
+//     INST(Not)                                                                  \
+//     INST(Xor)                                                                  \
+//     INST(PushImmediate)                                                        \
+//     INST(PushAddress)                                                          \
+//     INST(PushRegister)                                                         \
+//     INST(Pop)                                                                  \
+//     INST(Increment)                                                            \
+//     INST(Decrement)                                                            \
+//     INST(AddImmediate)                                                         \
+//     INST(AddAddress)                                                           \
+//     INST(AddRegister)                                                          \
+//     INST(SubImmediate)                                                         \
+//     INST(SubAddress)                                                           \
+//     INST(SubRegister)                                                          \
+//     INST(MulImmediate)                                                         \
+//     INST(MulAddress)                                                           \
+//     INST(MulRegister)                                                          \
+//     INST(DivImmediate)                                                         \
+//     INST(DivAddress)                                                           \
+//     INST(DivRegister)                                                          \
+//     INST(ModImmediate)                                                         \
+//     INST(ModAddress)                                                           \
+//     INST(ModRegister)                                                          \
+//     INST(CompareImmediate)                                                     \
+//     INST(CompareAddress)                                                       \
+//     INST(CompareRegister)                                                      \
+//     INST(Jump)                                                                 \
+//     INST(JumpIfEquals)                                                         \
+//     INST(JumpIfNotEquals)                                                      \
+//     INST(Subroutine)                                                           \
+//     INST(Return)                                                               \
+//     INST(Halt)                                                                 \
+//     INST(Label)
+//
+
 #define LIST_OF_INSTRUCTIONS                                                   \
-    INST(Nop)                                                                  \
     INST(LoadImmediate)                                                        \
     INST(LoadAddress)                                                          \
-    INST(LoadRegister)                                                         \
-    INST(StoreImmediate)                                                       \
-    INST(StoreAddress)                                                         \
-    INST(StoreRegister)                                                        \
-    INST(ShiftLeft)                                                            \
-    INST(ShiftRight)                                                           \
-    INST(And)                                                                  \
-    INST(Or)                                                                   \
-    INST(Not)                                                                  \
-    INST(Xor)                                                                  \
-    INST(PushImmediate)                                                        \
-    INST(PushAddress)                                                          \
-    INST(PushRegister)                                                         \
-    INST(Pop)                                                                  \
-    INST(Increment)                                                            \
-    INST(Decrement)                                                            \
-    INST(AddImmediate)                                                         \
-    INST(AddAddress)                                                           \
-    INST(AddRegister)                                                          \
-    INST(SubImmediate)                                                         \
-    INST(SubAddress)                                                           \
-    INST(SubRegister)                                                          \
-    INST(MulImmediate)                                                         \
-    INST(MulAddress)                                                           \
-    INST(MulRegister)                                                          \
-    INST(DivImmediate)                                                         \
-    INST(DivAddress)                                                           \
-    INST(DivRegister)                                                          \
-    INST(ModImmediate)                                                         \
-    INST(ModAddress)                                                           \
-    INST(ModRegister)                                                          \
-    INST(CompareImmediate)                                                     \
-    INST(CompareAddress)                                                       \
-    INST(CompareRegister)                                                      \
-    INST(Jump)                                                                 \
-    INST(JumpIfEquals)                                                         \
-    INST(JumpIfNotEquals)                                                      \
-    INST(Subroutine)                                                           \
-    INST(Return)                                                               \
-    INST(Halt)                                                                 \
-    INST(Label)
+    INST(LoadRegister)
 
 namespace Assembler {
 
@@ -70,13 +76,10 @@ private:
     void emit8(Arch::Luna16);
     void emit16(std::uint16_t);
 
-#define INST(name) void compile_inst(const Parser::name&, Arch::Luna16);
-    LIST_OF_INSTRUCTIONS
-#undef INST
-
     void compile_inst(const Parser::Immediate&, Arch::Luna16);
     void compile_inst(const Parser::Address&, Arch::Luna16);
     void compile_inst(const Parser::Register&, Arch::Luna16);
+    void compile_inst(const Parser::Label&, Arch::Luna16);
 
 private:
     std::vector<std::uint8_t>& m_output;
