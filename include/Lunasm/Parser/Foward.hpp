bda8caf7aab3f2dbc9f54939c6ff77ff4c7b91b7
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-11-02 21:43:07 -0300

    chore: add a foward file to the parser

---
+++ include/Lunasm/Parser/Foward.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Bitwise.hpp"
+#include "Compare.hpp"
+#include "Halt.hpp"
+#include "Jump.hpp"
+#include "Label.hpp"
+#include "Load.hpp"
+#include "Math.hpp"
+#include "Nop.hpp"
+#include "Stack.hpp"
+#include "Store.hpp"
+#include "Subroutine.hpp"
+
+#include "Address.hpp"
+#include "Immediate.hpp"
+#include "Register.hpp"
+
+namespace Parser {} // namespace Parser
