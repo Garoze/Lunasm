6567567f15532e41350421ae638bbe81fde39fa9
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-11-02 21:41:37 -0300

    feat: add intermediare class for register

---
+++ include/Lunasm/Parser/Register.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Parser/Instruction.hpp"
+#include "Parser/Operand.hpp"
+
+namespace Parser {
+
+class Register : public Instruction
+{
+public:
+    [[nodiscard]] const Operand& dst() const;
+    [[nodiscard]] const Operand& src() const;
+
+    virtual std::string as_string() const = 0;
+
+protected:
+    Register(Instruction::kind_t, Operand::value_t, Operand::value_t);
+
+    Operand m_dst;
+    Operand m_src;
+};
+
+} // namespace Parser
